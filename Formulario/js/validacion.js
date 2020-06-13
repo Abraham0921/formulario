dc619c063d4eaeeea5670c74f9d0c0637d090cae
Abraham <somer0921@gmail.com>
2020-06-13 08:19:32 -0500

    agregamos validacion de edad en js

--- Formulario/js/validacion.js
+++ Formulario/js/validacion.js
@@ -20,9 +20,10 @@ function validar(formulario) {
         for (var j = 0; j < checkOk.length; j++)
             if (ch == checkOk.charAt(j))
                 break;
-        if (j == checkOk.length)
+        if (j == checkOk.length) {
             allValid = false;
-        break;
+            break;
+        }
 
     }
     //saber estado de valid
@@ -31,4 +32,36 @@ function validar(formulario) {
         formulario.nombre.focus();
         return false;
     }
+    if (formulario.edad.value.length < 2) {
+        alert("Escriba maximo 2 digitos o la maxima edad es de 99");
+        //focus nos ayuda  alocalizar donde esta el error del llenado
+        //realiza un posicionamiento al elemento del formulario
+        formulario.edad.focus();
+        return false
+    }
+    var checkOk = "1234567890";
+    var checkString = formulario.edad.value;
+    alert(checkString)
+    var allValid = true;
+
+    for (var i = 0; i < checkString.length; i++) {
+        //charAt  nos ayuda a separar una cadena en carcateres
+        //ejemplo Ana.... char A n a
+        var ch = checkString.charAt(i);
+        for (var j = 0; j < checkOk.length; j++)
+            if (ch == checkOk.charAt(j))
+                break;
+        if (j == checkOk.length) {
+            allValid = false;
+            break;
+        }
+
+    }
+    //saber estado de valid
+    if (!allValid) {
+        alert("Escriba solo numero en el campo Edad");
+        formulario.edad.focus();
+        return false;
+    }
+
 }
