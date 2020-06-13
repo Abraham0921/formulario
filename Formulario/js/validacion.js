b425fba3cb90a84970fc328aa854ba5bf81fa55d
Abraham <somer0921@gmail.com>
2020-06-13 08:06:18 -0500

    validacion de solo letras

--- Formulario/js/validacion.js
+++ Formulario/js/validacion.js
@@ -1,7 +1,7 @@
 // version 4 java script
 function validar(formulario) {
 
-    if (formulario.nombre.value.lenght < 3) {
+    if (formulario.nombre.value.length < 3) {
         alert("Escriba por lo menos 3 carcateres en el campo de nombre");
         //focus nos ayuda  alocalizar donde esta el error del llenado
         //realiza un posicionamiento al elemento del formulario
@@ -9,4 +9,27 @@ function validar(formulario) {
         return false
     }
 
+    var checkOk = "ABCDEFGHIJKLMÑNOPQRSTUVWXYZ" + "abcdefghijklmnñopqrstuvwxyz";
+    var checkString = formulario.nombre.value;
+    alert(checkString)
+    var allValid = true;
+    for (var i = 0; i < checkString.length; i++) {
+        //charAt  nos ayuda a separar una cadena en carcateres
+        //ejemplo Ana.... char A n a
+        var ch = checkString.charAt(i);
+        for (var j = 0; j < checkOk.length; j++)
+            if (ch == checkOk.charAt(j))
+                break;
+        if (j == checkOk.length) {}
+        allValid = false;
+        break;
+    }
+
+}
+//saber estado de valid
+if (!allValid) {
+    alert("Escriba letras en el formulario de Nombre");
+    formulario.nombre.focus();
+    return false;
+}
 }
