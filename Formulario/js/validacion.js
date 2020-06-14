3c68fbe5384087bdc7c9859b7767cd1fc1b8d242
Abraham <somer0921@gmail.com>
2020-06-13 20:29:30 -0500

    agregamos fecha a formulario4

--- Formulario/js/validacion.js
+++ Formulario/js/validacion.js
@@ -72,8 +72,8 @@ function validar(formulario) {
         return false
     }
     var fecha = formulario.fecha.value;
-    var day = formulario.fecha.value.split("- ")
-    alert("fecha:" + day)
+    var day = formulario.fecha.value.split(" - ")
+    alert("dia:" + day)
     var txt = formulario.correo.value;
     //expresion regular
     var b = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/
