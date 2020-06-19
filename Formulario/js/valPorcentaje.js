aa8541dae2595ca143e4aa08631519f8a4f975e1
Abraham <somer0921@gmail.com>
2020-06-18 21:10:34 -0500

    codigo para valPorcentaj.js

--- Formulario/js/valPorcentaje.js
+++ Formulario/js/valPorcentaje.js
@@ -1,11 +1,9 @@
 function valPorcentaje(formulario) {
-    if (formulario.hombres.value.length == null ||
-        formulario.mujeres.value.length == null) {
+    if (formulario.hombres.value.length == null) {
         alert("No deven de estar los campos vacios!");
         //focus nos ayuda  alocalizar donde esta el error del llenado
         //realiza un posicionamiento al elemento del formulario
         formulario.hombres.focus();
-        formulario.mujeres.focus();
         return false
     }
 }
