384f44bdd0223b5343b516809a2c67082f6ff798
Abraham <somer0921@gmail.com>
2020-06-18 21:09:38 -0500

    creacion de porcentaje.html, valPorcentaje.js y style.css

---
+++ Formulario/js/valPorcentaje.js
@@ -0,0 +1,11 @@
+function valPorcentaje(formulario) {
+    if (formulario.hombres.value.length == null ||
+        formulario.mujeres.value.length == null) {
+        alert("No deven de estar los campos vacios!");
+        //focus nos ayuda  alocalizar donde esta el error del llenado
+        //realiza un posicionamiento al elemento del formulario
+        formulario.hombres.focus();
+        formulario.mujeres.focus();
+        return false
+    }
+}
